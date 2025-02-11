0f20546182f759dbc451bc2ff7026d3336b5ccb7
LKoushik2003 <koushikindore@gmail.com>
2025-02-11 09:55:27 +0530

    Add function to get user time and individual elements

--- popup.js
+++ popup.js
@@ -14,3 +14,17 @@ function getUserTimeZone() {
   
   const userTimeZone = getUserTimeZone(); 
   
+function getUserTime(){
+    const now = new Date();
+    const year = now.getUTCFullYear();
+    const month = String(now.getUTCMonth()+1).padStart(2,'0');
+    const date = String(now.getUTCDate()).padStart(2,'0');
+    const hour = String(now.getUTCHours()).padStart(2,'0');
+    const minutes = String(now.getUTCMinutes()).padStart(2,'0');
+    
+    //holding seconds part of timer
+    //const seconds = String(now.getUTCSeconds()).padStart(2,'0');
+    console.log(now +' '+ year+' ' + month+' ' + date+' ' + hour+' ' + minutes)
+}
+
+getUserTime();
